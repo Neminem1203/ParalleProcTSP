c7da705c42431e214a0e5647f8e3a4fe122b121c
neminem1203 <tpaul1203>
2017-07-19 18:05:28 -0400

    FINALLY! PARALLELIZED! OpenMP and MPI

--- TSP.cpp
+++ TSP.cpp
@@ -1,124 +1,108 @@
 #include <vector>
 #include <iostream>
-//#include <thread>
-//#include <time.h>
-#include <stdlib.h>
 #include <omp.h>
+#include <mpi.h>
+#include <time.h>
 using namespace std;
-//NON-Recursive Version of TSP
 
-//number of nodes (including node 0) (n > 1)
-const int n = 7;
+const int seed = 150;
+const int n = 8;
+const int smallestValue = 30;
 
-//Matrix in textbook
-/*	int adjMat[n][n] = {
-{ 0, 1, 3, 8 },
-{ 5, 0, 2, 6 },
-{ 1,18, 0,10 },
-{ 7, 4,12, 0 }
-};
-*/
-
-//calculate different combos (creating combo list)
-void com(vector<vector<int> > &list, int n, int iters) {
-	for (int i = 0; i < iters; i++) {
-		vector<int>::iterator iter = list[i].begin();
-		list[i].insert(iter + (i%n), n);
-	}
-}
-		//adjacency matrix, combination list		, iteration of combo list, this path 
-int DFS(int adjMat[n][n], vector<vector<int> > comList, int i) {
-	int sum = 0;
-	for (int j = 0; j < n; j++) {
-			sum += adjMat[ comList[i][j] ][ comList[i][j+1] ];
-	}
-	return sum;
+void com( vector < vector < int > > &list, int n, int iters){
+    for(int i = 0; i < iters; i++){
+        vector<int>::iterator iter =list[i].begin();
+        list[i].insert(iter + (i % n), n);
+    }
 }
 
-int main() {
-	vector<int> shortestPath;
-	int smallestSum = 30 * n; //max int value
-	int sum;
-		//cout << tid << " of " << nthreads << endl;
-		srand(150);
+int DFS(int adjMat[n][n], vector < vector < int > > comList, int i){
+    int sum = 0;
+    for(int j = 0; j < n; j++){
+        sum += adjMat[ comList[i][j] ][ comList[i][j + 1] ];
+    }
+    return sum;
+}
 
-		int adjMat[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i != j)
-				{
-					adjMat[i][j] = (rand() % 30) + 1;
-				}
-				else
-				{
-					adjMat[i][j] = 0;
-				}
-			}
-		}
+int main(int argc, char * argv[]){
+    MPI_Init(&argc, &argv);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
+    int *sums;
+    if(world_rank == 0){
+        sums = (int *)malloc(sizeof(int) * world_size);
+    }
+    srand( seed );
 
-		//	clock_t begin = clock();
-			//calculate amount of different combos in TSP (n! number of iterations)
+    int adjMat[n][n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i != j)
+                {adjMat[i][j] = (rand() % smallestValue) + 1;}
+            else
+                {adjMat[i][j] = 0;}
+        }
+    }
 
-		int iters = 1;
-		for (int i = 1; i < n; i++) {
-			iters *= i;
-		}
+    int tid, nthreads;
+    vector<int> shortestPath;
+    int smallestSum = (smallestValue * (n + 1)) + 1;
+    int sum;
 
-		//Vector of different combos
-		vector<vector<int> > comList(iters);
+    int iters = 1;
+    for(int i = 1; i < n; i++)
+        {iters *= i;}
+    vector< vector < int > > comList(iters);
 
-
-		//Different Combinations 
-		for (int i = 1; i < n; i++) {
-			com(comList, i, iters);
-		}
-
-		//Push 0 in the front and back of list (we want to leave and reeneter the 0th node)
-		for (unsigned int i = 0; i < comList.size(); i++) {
-			vector<int>::iterator it = comList[i].begin();
-			comList[i].insert(it, 0);
-			comList[i].push_back(0);
-		}
-
-		//Output
-		/*
-		for (int i = 0; i < comList.size(); i++) {
-			for (int j = 0; j < comList[i].size(); j++) {
-				cout << comList[i][j] << " ";
-			}
-			cout << endl;
-		}*/
-
-
+    for(int i = 1; i < n; i++)
+        {com(comList, i, iters);}
+    
+    for(int i = 0; i < comList.size(); i++){
+        vector<int>::iterator it = comList[i].begin();
+        comList[i].insert(it, 0);
+        comList[i].push_back(0);
+    }
 #pragma omp parallel
-	{
-		int tid;
-		tid = omp_get_thread_num();
-		int nthreads = omp_get_num_threads();
-		for (unsigned int i = 0; i < comList.size(); i++) {
-			//adjancecy matrix, combo list, iteration in combo list,
-			sum = DFS(adjMat, comList, i);
-	/*		for (unsigned int x = 0; x < comList[i].size(); x++) {
-				cout << comList[i][x] << ", ";
-			}
-			cout << "\tSum: " << sum << endl;
-	*/		if (smallestSum > sum) {
-				smallestSum = sum;
-				shortestPath = comList[i];
-			}
-		}
-	}
-	cout << "Shortest Path: ";
-	for (unsigned int i = 0; i < shortestPath.size(); i++) {
-		cout << shortestPath[i] << ", ";
-	}
-	cout << "\nSum: " << smallestSum << endl;;
+{
+    tid = omp_get_thread_num();
+    nthreads = omp_get_num_threads();
+    for(int i = (nthreads * world_rank) + tid; i < comList.size(); i += (nthreads * world_size)){
+        sum = DFS(adjMat, comList, i);
+        if(smallestSum > sum){
+            smallestSum = sum;
+            shortestPath = comList[i];
+        }
 
-//	clock_t end = clock();
+    }
 
-//	cout << "Clock: " << ((float)(end - begin) / CLOCKS_PER_SEC) << endl;
-
-	//Pause
-	return 0;
+}//end of parallel omp 
+    unsigned int lowest = n;
+    MPI_Gather(&smallestSum, 1, MPI_INT, sums, 1 , MPI_INT, 0, MPI_COMM_WORLD);
+    int sSOA = 30 * (n + 1) + 1; //smallestSumOfAll
+    if(world_rank == 0){
+    for(int i = 0; i < world_size; i++){
+        if(sums[i] < sSOA){
+            lowest = i;
+            sSOA = sums[i];
+        }
+    }   
+        for(int i = 1; i < world_size; i++){
+            MPI_Send(&lowest, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+    }
+else{MPI_Recv(&lowest, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);}
+    
+    if(world_rank == lowest){
+        cout<<"Lowest Sum: "<<smallestSum<<endl;
+        cout<<"Shorest Path: ";
+        for(int i = 0; i < shortestPath.size(); i++){
+            cout << shortestPath[i] <<", ";
+        }
+        cout<<"\nFrom Rank: "<<world_rank<<endl;
+        }
+    MPI_Finalize();
+    return 0;
 }
-
